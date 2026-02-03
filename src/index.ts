8d02152cb4c9828f0e82838c5f87f8e4645776bf
Patrick Hyatt <patrickhyatt@duck.com>
2026-02-03 08:13:28 -0500

    Change header color and update HTML content
    Updated the HTML content and style for better presentation.

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,23 @@ export default {
 	async fetch(request): Promise<Response> {
 		let html_content = "";
 		let html_style =
-			"body{padding:6em; font-family: sans-serif;} h1{color:#f6821f;}";
+			"body{padding:6em; font-family: sans-serif;} h1{color:#a62b2b;}";
 
-		html_content += "<p> Colo: " + request.cf.colo + "</p>";
 		html_content += "<p> Country: " + request.cf.country + "</p>";
 		html_content += "<p> City: " + request.cf.city + "</p>";
+		html_content += "<p> Region: " + request.cf.region + "</p>";
 		html_content += "<p> Continent: " + request.cf.continent + "</p>";
 		html_content += "<p> Latitude: " + request.cf.latitude + "</p>";
 		html_content += "<p> Longitude: " + request.cf.longitude + "</p>";
 		html_content += "<p> PostalCode: " + request.cf.postalCode + "</p>";
 		html_content += "<p> MetroCode: " + request.cf.metroCode + "</p>";
-		html_content += "<p> Region: " + request.cf.region + "</p>";
 		html_content += "<p> RegionCode: " + request.cf.regionCode + "</p>";
 		html_content += "<p> Timezone: " + request.cf.timezone + "</p>";
+		html_content += "<p> Colo: " + request.cf.colo + "</p>";
 
 		let html = `<!DOCTYPE html>
       <head>
-        <title> What's up doc </title>
+        <title> Where in the world </title>
         <style> ${html_style} </style>
 		<script>
 			const flag = { 
